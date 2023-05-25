7d5adcab593bd99006946d119cf524eae5f8d58a
sherryyu <bambooyu715@gmail.com>
2023-05-25 15:50:37 -0400

    uploading eod starter

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     Image app
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function Root() {
+  return (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
+
